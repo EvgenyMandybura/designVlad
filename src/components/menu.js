fdfeb5361ecb0a00337c6e1748c73119441dbeee
evgen <mandybura1994@gmail.com>
2023-05-10 22:55:54 +0300

    fix hover for menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,25 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import React from "react";
 
-const Menu = () => (
-  <div className="flex justify-between">
-    <p className="caption1 text-dark2">Chyhryn Vlad</p>
+const Menu = () => {
+  const { pathname } = useLocation();
 
-    <nav>
-      <ul className="flex flex-row">
-        <li className="caption1 text-dark2 mr-10 linkHover">
-          <Link to="/">Projects</Link>
-        </li>
-        <li className="caption1 text-dark2 mr-10 linkHover">
-          <Link to="/projects">Workflow</Link>
-        </li>
-        <li className="caption1 text-dark2 linkHover">
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+  return (
+    <div className="flex justify-between">
+      <p className={`caption1 text-dark2 ${pathname !== "/" && "linkHover"}`}>
+        <Link to="/">Chyhryn Vlad</Link>
+      </p>
+
+      <nav>
+        <ul className="flex flex-row">
+          <li
+            className={`caption1 text-dark2 mr-10 ${
+              pathname !== "/" && "linkHover"
+            }`}
+          >
+            <Link to="/">Projects</Link>
+          </li>
+          <li className="caption1 text-dark2 mr-10 linkHover">
+            <Link to="/projects">Workflow</Link>
+          </li>
+          <li className="caption1 text-dark2 linkHover">
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Menu;
